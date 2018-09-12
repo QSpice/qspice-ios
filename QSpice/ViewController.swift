d59d590d02badbab400d23c3b91ffc3929c4e9df
Anthony <fioritoanthony@outlook.com>
2018-09-12 14:04:21 -0400

    Added single pay application boilerplate includes cocoapods for swiftlint

---
+++ QSpice/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  QSpice
+//
+//  Created by Anthony Fiorito on 2018-09-12.
+//  Copyright © 2018 Anthony Fiorito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
